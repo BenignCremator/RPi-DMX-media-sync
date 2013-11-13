9ca5fea5abe69fee19222249f4a96cdf001d637b
Hunter Heinlen <dracus@speakeasy.net>
2013-11-13 17:37:01 -0500

    Update install_ola.sh

--- scripts/install_ola.sh
+++ scripts/install_ola.sh
@@ -4,9 +4,19 @@ chmod 777 /etc/apt
 echo "deb http://apt.openlighting.org/raspbian wheezy main" >> /etc/apt/sources.list
 chmod 755 /etc/apt
 apt-get update
+apt-get install ethtool
 apt-get install ola ola-python ola-rdm-tests
-adduser pi olad
 
+if [ `grep -c olad /etc/passwd` ]
+then
+    adduser --system --home /home/olad --shell /bin/bash olad
+fi
+
+if [ ! -d /home/olad ]
+then
+    mkdir /home/olad
+    chown olad:olad /home/olad
+fi
 
 /etc/init.d/olad start
 /etc/init.d/olad status
